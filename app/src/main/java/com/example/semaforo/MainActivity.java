32c76c9df761d1e2a2b509732fc78417da338930
G34A4D0 <gerar.210804@gmail.com>
2025-08-28 01:38:59 -0400

    Fase 4

--- app/src/main/java/com/example/semaforo/MainActivity.java
+++ app/src/main/java/com/example/semaforo/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     Button btn_cambiar;
 
     int bucle = 0;
-    ImageView imagen1, imagen2;
+    ImageView imagen1,imagen2, imagen3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         btn_cambiar = findViewById(R.id.btn_cambiar);
         imagen1 = findViewById(R.id.imagen1);
         imagen2 = findViewById(R.id.imagen2);
+        imagen3 = findViewById(R.id.imagen3);
 
         btn_cambiar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,57 +46,11 @@ public class MainActivity extends AppCompatActivity {
                             runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
-                                    imagen1.setImageResource(R.drawable.amarillo);
+                                    imagen1.setImageResource(R.drawable.gris);
                                 }
                             });
 
-                            for (int i = 0; i < 5; i++) {
-                                try {
-                                    Thread.sleep(1000);
-                                } catch (InterruptedException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    imagen1.setImageResource(R.drawable.verde);
-                                }
-                            });
 
-                            for (int i = 0; i < 5; i++) {
-                                try {
-                                    Thread.sleep(1000);
-                                } catch (InterruptedException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    imagen1.setImageResource(R.drawable.rojo);
-                                }
-                            });
-
-                            bucle++;
-                        }
-                    }
-                });
-                thread1.start();
-
-                // Segundo hilo: controla imagen2
-                Thread thread2 = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        while (bucle <= 20) {
-
-                            for (int i = 0; i < 5; i++) {
-                                try {
-                                    Thread.sleep(1000);
-                                } catch (InterruptedException e) {
-                                    e.printStackTrace();
-                                }
-                            }
                             runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
@@ -113,7 +68,8 @@ public class MainActivity extends AppCompatActivity {
                             runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
-                                    imagen2.setImageResource(R.drawable.rojo);
+                                    imagen2.setImageResource(R.drawable.gris);
+                                    imagen3.setImageResource(R.drawable.verde);
                                 }
                             });
 
@@ -127,7 +83,8 @@ public class MainActivity extends AppCompatActivity {
                             runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
-                                    imagen2.setImageResource(R.drawable.verde);
+                                    imagen3.setImageResource(R.drawable.gris);
+                                    imagen1.setImageResource(R.drawable.rojo);
                                 }
                             });
 
@@ -135,7 +92,8 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 });
-                thread2.start();
+                thread1.start();
+
             }
         });
     }
